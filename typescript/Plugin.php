3b6eecb97071c6804200bed365230c2c2bf871e8
Joppe Aarts <joppe@zicht.nl>
2016-08-16 10:45:07 +0200

    initial commit

---
+++ typescript/Plugin.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author Joppe Aarts <joppe@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Tool\Plugin\Watch;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Zicht\Tool\Plugin as BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @package Zicht\Tool\Plugin\Watch
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * @param ArrayNodeDefinition $rootNode
+     * @return void
+     */
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('typescript')
+                    ->children()
+                        ->scalarNode('bin')
+                            ->defaultValue('./node_modules/typescript/bin/tsc')
+                            ->beforeNormalization()
+                                ->always(function($v) {
+                                    if (is_file($v)) {
+                                        return realpath($v);
+                                    }
+                                })
+                                ->end()
+                            ->validate()
+                                ->ifTrue(function($f) {
+                                    return !is_file($f);
+                                })
+                                ->thenInvalid('File does not exist. Do you need to install typescript? npm install typescript')
+                            ->end()
+                        ->end()
+                        ->scalarNode('source')->defaultValue('htmldev/src')->end()
+                        ->scalarNode('typings_bin')
+                            ->defaultValue('./node_modules/typings/dist/bin.js')
+                            ->beforeNormalization()
+                                ->always(function($v) {
+                                    if (is_file($v)) {
+                                        return realpath($v);
+                                    }
+                                })
+                                ->end()
+                            ->validate()
+                                ->ifTrue(function($f) {
+                                    return !is_file($f);
+                                })
+                                ->thenInvalid('File does not exist. Do you need to install typings? npm install typings')
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
